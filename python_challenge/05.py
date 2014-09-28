3d40686e1e03f04d6079e6a8e5d09660af9b3526
espooky7 <emily.dahlberg26@gmail.com>
2014-09-28 16:37:36 -0700

    edited comments

--- python_challenge/05.py
+++ python_challenge/05.py
@@ -1,12 +1,10 @@
+
 """
 I have to admit, I 100% cheated on this one. I had ABSOLUTELY no clue where to even start.
 The only thing I got on my own was the word 'pickle', which led me to yes! pickle!. With some hints
 I was able to find the banner.p, but I was at a complete loss on how to handle it.
 
-I'm still not completely sure what some of the hints are referring to in the hints forum.
-
-I think I can chalk this one up to not knowing a single thing about UNIX or pickling. I dearly hope
-the following challenges won't be this inaccessible to me.
+Some of the trouble is due to me being in the wrong hints forum the entire time.
 
 Without further ado:
 """
